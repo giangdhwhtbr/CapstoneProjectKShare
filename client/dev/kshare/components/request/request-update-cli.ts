f80baebe6ed854a4ec8ec5049ae10c866cfa96b3
AnhNQSE03216 <quanganh31394@gmail.com>
2016-06-01 00:19:21 +0700

    print

---
+++ client/dev/kshare/components/request/request-update-cli.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit, Inject } from 'angular2/core';
+import { Request } from '../../../dashboard/interface/request';
+import { RequestService } from '../../../dashboard/services/requests-service';
+import { HeaderComponent } from '../shared/header';
+import { FooterComponent } from '../shared/footer';
+import { SideBarComponent } from '../shared/sidebar';
+import { FriendListComponent} from '../shared/friend-list';
+import { Router, ROUTER_DIRECTIVES, ROUTER_PROVIDERS, RouteConfig, RouteParams} from'angular2/router';
+import { FORM_DIRECTIVES, FormBuilder, ControlGroup, Control, AbstractControl  } from 'angular2/common';
+
+@Component({
+  selector: 'request-update-cli',
+  templateUrl: 'client/dev/kshare/templates/request-cli/request-update-cli.html',
+  styleUrls: [],
+  directives: [FORM_DIRECTIVES, ROUTER_DIRECTIVES, SideBarComponent, FooterComponent, HeaderComponent]
+})
+
+export class RequestUpdateClientComponent {
+  updateRequestFormCli: ControlGroup;
+  
+  id: string;
+  
+  request: Request;
+  _id: string;
+  title: string;
+  description: string;
+  
+  ngOnInit():void {
+    this._requestService.getRequestById(this.id).subscribe(
+      (request) => {
+        this.request = request;
+        this.title = request.title;
+        this.description = request.description;
+        this._id = request._id;
+    },
+      (error) => {
+        console.log(error.text());
+      }
+    );
+  }
+  
+  constructor(@Inject(FormBuilder) fb: FormBuilder, @Inject(RequestService) private _requestService: RequestService,
+            public router: Router, rParam: RouteParams) {
+    this.id = rParam.get('id');
+    
+    this.updateRequestFormCli = fb.group({
+      "_id": [""],
+      "title": [""],
+      "description": [""]
+    });
+  }
+  
+  updateRequest(request) {
+    this._requestService.updateRequest(request).subscribe((request)=> {
+      console.log('update successed');
+    },
+    (error) => {
+      console.log(error.text());
+    }
+    );
+    window.location.href = '/requests/'+this.id;
+  }
+
+}
---
+++ client/dev/kshare/components/shared/sidebar.ts
@@ -0,0 +1,44 @@
+import {
+  Component,
+  Inject,
+  OnInit
+} from 'angular2/core';
+import { Knowledge } from '../../../dashboard/interface/knowledge';
+import { KnowledgeService } from '../../../dashboard/services/knowledge-service';
+@Component({
+  selector : 'sidebar',
+  templateUrl: 'client/dev/kshare/templates/shared/sidebar.html',
+  styleUrls: ['client/dev/kshare/styles/sidebar.css'],
+  directives: []
+})
+
+export class SideBarComponent {
+  constructor(private _knowledgeService: KnowledgeService){
+
+  }
+
+  knowledges: Knowledge[];
+
+  ngOnInit(): void {
+    this._knowledgeService.getAllKnowledges().subscribe((knowledges) => {
+      var parent = [];
+      var subCate = [];
+      for(var i = 0;i < knowledges.length;i++){
+        if(!knowledges[i].hasOwnProperty('parent')){
+          parent.push(knowledges[i]);
+        }
+      }
+      for(var i = 0;i<parent.length;i++){
+        for(var j = 0;j< knowledges.length;j++){
+          if((knowledges[j].hasOwnProperty('parent'))&&(knowledges[j].parent===parent[i]._id)){
+            subCate.push(knowledges[j]);
+          }
+        }
+        parent[i]["subCategory"] = subCate;
+        subCate = [];
+      }
+      this.knowledges = parent;
+    });
+  }
+
+}
