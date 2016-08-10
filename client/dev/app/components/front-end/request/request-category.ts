a1d37b0d7394473ba765f4f2f5d5956521f83ce4
giangdhwhtbr <giangdhwhtbr@gmail.com>
2016-08-10 22:33:15 +0700

    update routerLink

--- client/dev/app/components/front-end/request/request-category.ts
+++ client/dev/app/components/front-end/request/request-category.ts
@@ -3,13 +3,16 @@ import {
   OnInit,
   Pipe,
   PipeTransform,
-  Inject
+  Inject,
+  OnDestroy
 } from '@angular/core';
 import { ROUTER_DIRECTIVES, Router, ActivatedRoute} from '@angular/router';
 import { Request } from '../../../interface/request';
 import { Knowledge } from '../../../interface/knowledge';
 
 import { RequestService } from '../../../services/requests';
+
+import { Subscription }       from 'rxjs/Subscription';
 
 @Component({
   selector: 'request-category-cli',
@@ -26,78 +29,76 @@ export class RequestCategoryComponent {
   type: string;
   requests: Request[];
   knowledges: Knowledge[];
+  private sub: Subscription;
   constructor(private _requestService: RequestService, public router: Router,
     private route: ActivatedRoute) {
-    this.route
+  }
+
+  ngOnInit(): void {
+
+    this.sub = this.route
       .params
       .subscribe(params => {
         let type = params['type'];
         let id = params['id'];
+
+        //get templates from children category
+        if (type === "subcategory") {
+          this._requestService.getRequestByKnowledgeId(id).subscribe(
+            (requests) => {
+              if (requests.length == 0) {
+                this.isExistRecord = true;
+              }
+              else {
+                this.isExistRecord = false;
+              }
+              for (var i = 0; i < requests.length; i++) {
+                requests[i].createdAt = new Date(requests[i].createdAt);
+                requests[i].modifiedDate = new Date(requests[i].modifiedDate);
+              }
+              this.requests = requests;
+            });
+        }
+
+        //get templates from parent category
+        if (type === "category") {
+          this._requestService.getKnowledgeByParent(id).subscribe(
+            (knowledges) => {
+
+              var a = [];
+              this.knowledges = knowledges;
+              for (var i = 0; i < this.knowledges.length; i++) {
+                this._requestService.getRequestByKnowledgeId(this.knowledges[i]._id).subscribe(
+                  (requests) => {
+                    //for each child knowledge get requests
+                    for (var j = 0; j < requests.length; j++) {
+                      a.push(requests[j]);
+                    }
+
+                    for (var i = 0; i < a.length; i++) {
+                      a[i].createdAt = new Date(requests[i].createdAt);
+                      a[i].modifiedDate = new Date(requests[i].modifiedDate);
+                      if (requests[i].status === 'pending') {
+                        requests[i].status = 'Đang chờ';
+                      }
+                    }
+                    if (a.length == 0) {
+                      this.isExistRecord = true;
+                    }
+                    else {
+                      this.isExistRecord = false;
+                    }
+                    this.requests = a;
+                  });
+              }
+            },
+            (Error) => {
+              console.log(Error);
+            });
+        }
       });
-
   }
-
-  ngOnInit(): void {
-    //get templates from children category
-    if (this.type === "subcategory") {
-      this._requestService.getRequestByKnowledgeId(this.id).subscribe(
-        (requests) => {
-          if (requests.length == 0) {
-            this.isExistRecord = true;
-          }
-          else {
-            this.isExistRecord = false;
-          }
-          for (var i = 0; i < requests.length; i++) {
-            requests[i].createdAt = new Date(requests[i].createdAt);
-            requests[i].modifiedDate = new Date(requests[i].modifiedDate);
-          }
-          this.requests = requests;
-        });
-    }
-
-    //get templates from parent category
-    if (this.type === "category") {
-      this._requestService.getKnowledgeByParent(this.id).subscribe(
-        (knowledges) => {
-
-          var a = [];
-          this.knowledges = knowledges;
-          for (var i = 0; i < this.knowledges.length; i++) {
-            this._requestService.getRequestByKnowledgeId(this.knowledges[i]._id).subscribe(
-              (requests) => {
-                //for each child knowledge get requests
-                for (var j = 0; j < requests.length; j++) {
-                  a.push(requests[j]);
-                }
-
-                for (var i = 0; i < a.length; i++) {
-                  a[i].createdAt = new Date(requests[i].createdAt);
-                  a[i].modifiedDate = new Date(requests[i].modifiedDate);
-                  if (requests[i].status === 'pending') {
-                    requests[i].status = 'Đang chờ';
-                  }
-                }
-                if (a.length == 0) {
-                  this.isExistRecord = true;
-                }
-                else {
-                  this.isExistRecord = false;
-                }
-                this.requests = a;
-              });
-          }
-        },
-        (Error) => {
-          console.log(Error);
-        });
-    }
-
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
-<<<<<<< HEAD:client/dev/app/components/front-end/request/request-search.ts
-  requests: Request[];
-  knowledges: Knowledge[];
-=======
-
->>>>>>> 30e2669ffc9bed2d630355ba76e64aa4cf53042f:client/dev/app/components/front-end/request/request-category.ts
 }
